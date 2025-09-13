2b9f514572d66165842999ad123c0fc8022ea9a3
BackToTheDev <rnjswnstjd13@gmail.com>
2025-09-13 15:14:41 +0900

    데이터를 Client에 반환하는 방법

---
+++ src/main/java/com/example/demo1/response/ResponseController.java
@@ -0,0 +1,29 @@
+package com.example.demo1.response;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/response")
+public class ResponseController {
+    // Content-Type: text/html
+    // Response Body
+    // {"name":"Robbie", "age",:95
+    @GetMapping("/json/string")
+    @ResponseBody
+    public String helloStringJson() {
+        return "{\"name\":\"Robbie\",\"age\":95}";
+    }
+
+    // ConTent-Type: application/json
+    //Response Body
+    //{"name":"Robbie", "age":95}
+    @GetMapping("/json/class")
+    @ResponseBody
+    public Star helloClass() {
+        return new Star("Robbie", 95);
+    }
+}
